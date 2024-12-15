4adf20073b4963a4d81d8f4f85cc4ffd2a4e0c8c
shantouly <22yli10@stu.edu.cn>
2024-12-15 18:18:38 +0800

    更新对象池模式中不使用对象池的方法

--- Assets/ObjectPoolPattern/GameManager.cs
+++ Assets/ObjectPoolPattern/GameManager.cs
@@ -11,6 +11,27 @@ public class GameManager : MonoBehaviour
 		ObjectPool.Instance.PreLoad(testObject,500);
 	}
 	
+	public void TestNotOP()
+	{
+		StartCoroutine(CreateObjNotOP());
+	}
+	
+	private IEnumerator CreateObjNotOP()
+	{
+		float t = 0;
+		for(int i =0;i < 500;i++)
+		{
+			int x = Random.Range(-30,30);
+			int y = Random.Range(-30,30);
+			int z = Random.Range(-30,30);
+			GameObject newObj = ObjectPool.Instance.GetObject(testObject,new Vector3(x,y,z),Quaternion.identity);
+			Destroy(newObj,2f);
+			yield return null;
+			t+=Time.deltaTime;
+		}
+		Debug.Log("所用的时间："+t);
+		
+	}
 	public void Test()
 	{
 		StartCoroutine(CreateObj());
